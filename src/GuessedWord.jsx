58d365558d8e7fba9c8962fa00bec5ea98b32c4b
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2020-11-22 13:28:07 -0600

    guessed words with guessed items implemented on test and component

--- src/GuessedWord.jsx
+++ src/GuessedWord.jsx
@@ -2,10 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const GuessedWord = (props) => {
+  const { guessedWords } = props;
   let contents;
-  if (props.guessedWords.length === 0) {
+  if (guessedWords.length === 0) {
     contents = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  } else {
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWords.map((word, index) => (
+              <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
   return <div data-test="component-guessed-words">{contents}</div>;
