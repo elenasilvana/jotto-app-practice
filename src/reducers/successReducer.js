9d5a9d119e2b8f8028cfd1b250980f10151e19a1
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2020-12-06 11:45:30 -0600

    test and function added to successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,11 @@
-export default (state, action) => {
-  return null;
+import { actionTypes } from "../actions";
+
+//state default to false
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
