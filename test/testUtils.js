c5def85a5c98a445bae7c5816fbf53381470d0ba
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2020-12-14 06:55:18 -0600

    added redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,14 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { middlewares } from "../src/configureStore";
 
 import rootReducer from "../src/reducers";
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const findByTestAttr = (wrapper, val) => {
