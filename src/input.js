d62020a9199decc6dd00facb269f3281fa1f508f
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2021-01-04 11:36:32 -0600

    added a test to input.test

--- src/input.js
+++ src/input.js
@@ -1,5 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
+//secretWord string
 
-export const Input = () => {
+export const Input = ({ secretWord }) => {
   return <div data-test="component-input"></div>;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
--- src/input.test.js
+++ src/input.test.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { findByTestAttr } from "../test/testUtils";
+import { checkProps, findByTestAttr } from "../test/testUtils";
 import { Input } from "./input";
+import { assertPropTypes } from "check-prop-types";
 
-const setup = () => shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
+};
 
 describe("Input component", () => {
   it("should render Input component without error", () => {
     const wrapper = setup();
     const component = findByTestAttr(wrapper, "component-input");
     expect(component.length).toBe(1);
+  });
+  it("should receive secretWord as prop, and it should be string", () => {
+    checkProps(Input, { secretWord: "party" });
   });
 });
